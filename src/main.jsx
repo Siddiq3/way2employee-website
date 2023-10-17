e86e2cdb716eaa22e51a493e5d6db5ad6c83b244
Siddiq3 <104221239+Siddiq3@users.noreply.github.com>
2023-10-17 22:22:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3251781230941397"
+     crossorigin="anonymous"></script>
     <BrowserRouter>
       <App />
     </BrowserRouter>
