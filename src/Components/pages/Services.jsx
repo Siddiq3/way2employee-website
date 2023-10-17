4abde9a93ff7b1e179b95b28db41a246077cb8a9
siddiq <siddiqkolimi.dev@gmail.com>
2023-10-17 12:07:56 +0530

    second commit

--- src/Components/pages/Services.jsx
+++ src/Components/pages/Services.jsx
@@ -2,107 +2,255 @@ import React from "react";
 
 export const Services = () => {
     return (
-        <>
-            <h1>Services</h1>
-            <div>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt
-                itaque, perferendis necessitatibus corrupti ea magni voluptatibus est
-                ipsa. Nulla praesentium dolore eos alias incidunt, odio et eaque, sunt
-                vitae corporis animi, itaque architecto. Sunt minima nostrum fugiat
-                quibusdam voluptate dolore illo cupiditate quam odio sequi. Consequatur
-                tempora ullam, ipsam molestias dolor dolores tempore. Molestias, error
-                porro tenetur delectus numquam animi aut quod consequuntur? Dolor sequi
-                quisquam ex, eaque explicabo, labore maxime veniam voluptas enim
-                recusandae architecto est voluptates optio libero officiis consectetur
-                possimus voluptate ipsam alias velit veritatis reprehenderit
-                repudiandae. Ullam praesentium inventore minima eius dolorem omnis fugit
-                facilis.
+        <div style={{
+            flex: 1,
+            borderRadius: 10
+        }} className='class10'>
+
+
+
+            <div style={{ margin: 55, padding: 10, marginTop: 60, borderRadius: 10, }}>
+
+
+                <h1 style={{ textAlign: 'center', color: 'rebeccapurple', fontSize: 40 }}>10TH MATHS EM</h1>
             </div>
-            <br />
-            <div>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-                libero laboriosam quia totam, ducimus dolorum velit voluptates ipsam ad
-                labore repellendus! Reprehenderit corporis labore totam est quas natus
-                non quis? Atque porro obcaecati error hic perspiciatis itaque
-                reprehenderit ut aperiam, ipsa, repellendus voluptatum, architecto sint
-                delectus. Dolorem, officia nostrum? Quae similique eveniet, animi
-                laborum impedit commodi, deleniti cum adipisci atque quidem culpa
-                incidunt placeat id rem dolorum sit eligendi sapiente ea sed, voluptate
-                voluptates minima pariatur ipsam delectus. Inventore deserunt
-                exercitationem repudiandae ducimus nihil, assumenda vel ex optio aut
-                voluptates nostrum dolor beatae in voluptatem. Adipisci excepturi
-                nesciunt ab soluta.
+
+            <div style={{ margin: 15, padding: 10, }}>
+
+                <p style={{ fontSize: 25, margin: 15, padding: 10, }} className='div1'>
+                    AP State Syllabus 10th Class Maths Textbook Solutions Study Material Guide Pdf Free Download
+                </p>
+                <h1 style={{ fontSize: 25, marginLeft: 25, fontFamily: 'monospace' }} >10th Class Maths Textbook Solutions Chapter 1 Real Numbers</h1>
             </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
+
+            <div style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}  >
+
+                <ul style={{ alignContent: 'center', justifyContent: 'center', fontSize: 22, margin: 20, marginLeft: 50, }} className='box'  >
+
+
+
+                    <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-ex-1-1/" className='a'> Chapter 1 Real Numbers Ex 1.1</a><br />
+                    <br />
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-ex-1-2/"  > Chapter 1 Real Numbers Ex 1.2</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-ex-1-3/"  > Chapter 1 Real Numbers Ex 1.3</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-ex-1-4/"  > Chapter 1 Real Numbers Ex 1.4</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-ex-1-5/"  >Chapter 1 Real Numbers Ex 1.5</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-optional-exercise/" > Chapter 1 Real Numbers Optional Exercise</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-1-intext-questions/" >Chapter 1 Real Numbers InText Questions</a><br /><br />
+                    </li>
+
+                </ul>
+
+
             </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
+            <div style={{ margin: 15, padding: 10, }}>
+
+
+                <h1 style={{ fontSize: 25, marginLeft: 25, fontFamily: 'monospace' }} >10th Class Maths Textbook Solutions Chapter 2Sets</h1>
             </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
+
+            <div style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}>
+
+                <ul style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}>
+
+
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-2-ex-2-1/"  > Chapter 2 Sets Ex 2.1</a><br />
+                        <br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-2-ex-2-2/"  > Chapter 2 Sets Ex 2.2</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-2-ex-2-3/"  > Chapter 2 Sets Ex 2.3</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-2-ex-2-4/"  > Chapter 2 Sets Ex 2.4</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-2-intext-questions/"  > Chapter 2 Sets InText Questions1</a><br /><br />
+                    </li>
+
+
+                </ul>
+
+
             </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
+
+
+            <div style={{ margin: 15, padding: 10, }}>
+
+
+                <h1 style={{ fontSize: 25, marginLeft: 25, fontFamily: 'monospace' }} >10th Class Maths  Chapter 3.Polynomials</h1>
             </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
+
+            <div style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}>
+
+                <ul style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, marginTop: 30 }}>
+
+
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-3-ex-3-1/"  > Chapter 3.Polynomials Ex 3.1</a><br />
+                        <br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-3-ex-3-2/"  > Chapter 3.Polynomials Ex 3.2</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-3-ex-3-3/"  > Chapter 3.Polynomials Ex 3.3</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-3-ex-3-4/" > Chapter 3.Polynomials Ex 3.4</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-3-optional-exercise/" >  Chapter 3 Polynomials Optional Exercise</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-3-intext-questions/"  > Chapter 3.Polynomials InText Questions</a><br /><br />
+                    </li>
+
+
+                </ul>
+
+
             </div>
-        </>
-    );
+
+
+            <div style={{ margin: 15, padding: 10, }}>
+
+
+                <h1 style={{ fontSize: 25, marginLeft: 25, fontFamily: 'monospace' }} >10th Class Maths  Chapter 4. Pair of Linear Equations in Two Variables</h1>
+            </div>
+
+            <div style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}>
+
+                <ul style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, marginTop: 30 }}>
+
+
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-4-ex-4-1/"  > Chapter 4  Pair of Linear Equations in Two Variables Ex 4.1</a><br />
+                        <br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-4-ex-4-2/"  > Chapter 4  Pair of Linear Equations in Two Variables Ex 4.2</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-4-ex-4-3/"  > Chapter 4  Pair of Linear Equations in Two Variables Ex 4.3</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-4-optional-exercise/" >  Chapter 4  Pair of Linear Equations in Two Variables Optional Exercise</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-4-intext-questions/"  > Chapter 4 Pair of Linear Equations in Two Variables InText Questions</a><br /><br />
+                    </li>
+
+
+                </ul>
+
+
+            </div>
+
+
+            <div style={{ margin: 15, padding: 10, }}>
+
+
+                <h1 style={{ fontSize: 25, marginLeft: 25, fontFamily: 'monospace' }} >10th Class Maths  Chapter 5.Quadratic Equations</h1>
+            </div>
+
+            <div style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}>
+
+                <ul style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, marginTop: 30 }}>
+
+
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-1/"  > Chapter 5.Quadratic Equations Ex 5.1</a><br />
+                        <br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-2/"  > Chapter 5.Quadratic Equations Ex 5.2</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-3/"  > Chapter 5.Quadratic Equations Ex 5.3</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-4/" > Chapter 5.Quadratic Equations Ex 5.4</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-optional-exercise/" >  Chapter 5 Quadratic Equations Optional Exercise</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-intext-questions/"  > Chapter 5.Quadratic Equations InText Questions</a><br /><br />
+                    </li>
+
+
+                </ul>
+
+
+            </div>
+
+
+            <div style={{ margin: 15, padding: 10, }}>
+
+
+                <h1 style={{ fontSize: 25, marginLeft: 25, fontFamily: 'monospace' }} >10th Class Maths  Chapter 5. Progressions</h1>
+            </div>
+
+            <div style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, }}>
+
+                <ul style={{ alignContent: 'center', justifyContent: 'center', fontSize: 25, margin: 20, marginLeft: 50, marginTop: 30 }}>
+
+
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-1/"  > Chapter 5  Progressions Ex 5.1</a><br />
+                        <br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-2/"  > Chapter 5  Progressions Ex 5.2</a><br /><br />
+                    </li>
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-3/"  > Chapter 5  Progressions Ex 5.3</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-ex-5-4/"  > Chapter 5  Progressions Ex 5.4</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-optional-exercise/" >  Chapter 5  Progressions Optional Exercise</a><br /><br />
+                    </li>
+
+                    <li>
+                        <a href="https://apboardsolutions.guru/ap-ssc-10th-class-maths-solutions-chapter-5-intext-questions/"  > Chapter54 Progressions InText Questions</a><br /><br />
+                    </li>
+
+
+                </ul>
+
+
+            </div>
+
+
+        </div >
+    )
 };
